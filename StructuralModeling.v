f8af491ecfa08217d8a1637fa26daf6268f1ae41
Talha-Moghul <talhamughal20.tm@gmail.com>
2023-09-12 16:40:08 +0500

    Added sample code models

---
+++ StructuralModeling.v
@@ -0,0 +1,12 @@
+module structuralmodel(out1,out2, in1, in2);
+
+input in1, in2;
+output out1 out2;
+
+wire and_out, or_out;
+
+and gate_and(and_out, in1, in2);
+or gate_or(or_out, in1, in2);
+xor gate_xor(out1, and_out, or_out);
+not gate_not(out2,in2);
+endmodule
