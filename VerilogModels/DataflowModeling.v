32225e0f85bad74a8f6137c37854e46d0b64b7d2
Talha-Moghul <talhamughal20.tm@gmail.com>
2023-09-12 16:59:20 +0500

    Adding various verilog data models

---
+++ VerilogModels/DataflowModeling.v
@@ -0,0 +1,11 @@
+module dataflowmodel (out1, out2, in1, in2);
+input in1, in2;
+output out1, out2;
+
+wire and_out, or_out;
+
+assign and_out = in1 & int2;
+assign or_out = in1 | in2;
+assign out1 = and_out ^ or_out;
+assign out2=~in2;
+endmodule
